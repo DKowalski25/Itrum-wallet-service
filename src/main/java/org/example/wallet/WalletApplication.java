187629ba15b4da8c9d9b9cd65ff72a077f6f839c
DKowalski25 <dshinoda25@gmail.com>
2025-08-16 21:20:35 +0500

    Deleted EndLoader. No longer in use

--- src/main/java/org/example/wallet/WalletApplication.java
+++ src/main/java/org/example/wallet/WalletApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WalletApplication {
     public static void main(String[] args) {
-        if (System.getenv("IS_DOCKER") == null) {
-            EnvLoader.loadEnv();
-        }
         SpringApplication.run(WalletApplication.class, args);
     }
 }
