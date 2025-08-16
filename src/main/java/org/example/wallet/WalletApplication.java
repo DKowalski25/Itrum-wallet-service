608e0c71d9754c180e20acff3e0ec791954ba10d
DKowalski25 <dshinoda25@gmail.com>
2025-08-16 23:32:54 +0500

    Refactored

--- src/main/java/org/example/wallet/WalletApplication.java
+++ src/main/java/org/example/wallet/WalletApplication.java
@@ -1,6 +1,5 @@
 package org.example.wallet;
 
-import org.example.wallet.util.EnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
